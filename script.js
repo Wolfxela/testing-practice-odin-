19d6ae6431347b85593de68bb05d17cd24d70b2f
Wolfxela <alexthegreitsas@gmail.com>
2023-10-20 01:10:36 +0300

    forgor to commit the others so now we have almost everything,reverse calculator and caesar cypher

--- script.js
+++ script.js
@@ -15,4 +15,79 @@ function capitalize(input)
 
     return output.slice(0,output.length -1)
 }
-export {capitalize}
+function reverseString(input)
+{
+    let output = ""
+    const wordArray = input.split("")
+    for(let i = wordArray.length; i>0;i--)
+    {
+        output += wordArray[i - 1]
+    }
+    return output
+
+}
+const calculator = (function(){
+    const add = function(num1,num2)
+    {
+        return num1 + num2
+    }
+    const subtract = function(num1,num2)
+    {
+        return num1 - num2
+    }
+    const multiply = function(num1,num2)
+    {
+        return num1 * num2 
+    }
+    const divide = function(num1,num2)
+    {
+            return num1/num2
+    }
+    return {add,subtract,multiply,divide}
+})();
+const cypher = (function(){
+    //here the efficient route would be having a binary tree to search for the shifted letter, but for example i'll be using a array, which is less efficient
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const checkAlph = function(letter,key)
+    {
+        for(let i = 0; i<alphabet.length;i++)
+        {
+            if(letter == alphabet[i])
+            {
+                if(i+key > alphabet.length - 1)
+                {
+                    newkey = (i+key) - alphabet.length
+                    return alphabet[newkey]
+                }
+                else if(i+key <= -1)
+                {
+                    newkey = alphabet.length - key
+                    return alphabet[newkey]
+                }
+                return alphabet[i+key]
+            }
+        }
+        return " "
+    }
+    const encode = function(input,key){
+        let output = ""
+        const wordArray = input.split("")
+        for(let i = 0; i<wordArray.length;i++)
+        {
+         output += checkAlph(wordArray[i],key)
+        }
+        return output
+    }
+    const decode = function(input,key)
+    {
+        let output = ""
+        const wordArray = input.split("")
+        for(let i = 0; i<wordArray.length;i++)
+        {
+         output += checkAlph(wordArray[i],key -(key * 2))
+        }
+        return output
+    }
+    return {encode,decode}
+})();
+export {capitalize,reverseString,calculator,cypher}
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import { capitalize } from "./script";
+import { capitalize,reverseString,calculator,cypher } from "./script";
 
 test('capitalize first letter of word even if the letter is already capitalized',()=>{
     expect(capitalize("Fox")).toBe("Fox")
@@ -13,4 +13,42 @@ test('capitalize first letter of word without changing other charracters',()=>{
 test('capitalize first letter of words when theres multiple ',()=>{
     expect(capitalize("fox in the box")).toBe("Fox In The Box")
 })
+
+test('reverse the string',()=>{
+    expect(reverseString('hello!')).toBe("!olleh")
+})
+test('reverse the capitals in the string',()=>{
+    expect(reverseString('hellOoOoO!')).toBe("!OoOoOlleh")
+})
+
+test('reverse even if it has numbers inside it',()=>{
+    expect(reverseString('hellOoOoO11112!')).toBe("!21111OoOoOlleh")
+})
+test('add 2 numbers together',()=>{
+    expect(calculator.add(2,3)).toBe(5)
+})
+
+test('multiply 2 numbers together',()=>{
+    expect(calculator.multiply(2,2)).toBe(4)
+})
+test('divide 2 numbers together',()=>{
+    expect(calculator.divide(2,2)).toBe(1)
+})
+test('subtract 2 numbers together',()=>{
+    expect(calculator.subtract(2,1)).toBe(1)
+})
+test('encode said words given the key 1',()=>{
+    expect(cypher.encode("hello i need lettuce",1)).toBe("ifmmp j offe mfuuvdf")
+})
+test('encode said words given the key 2',()=>{
+    expect(cypher.encode("hello i need lettuce",2)).toBe("jgnnq k pggf ngvvweg")
+})
+test('decode said words given the key 1',()=>{
+    expect(cypher.decode("ifmmp j offe mfuuvdf",1)).toBe("hello i need lettuce")
+})
+test('decode said words given the key 1',()=>{
+    expect(cypher.decode("jgnnq k pggf ngvvweg",2)).toBe("hello i need lettuce")
+})
+
+
 
